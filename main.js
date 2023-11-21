64f941ecde92376e07de60764c855c4473995557
sanchu-git <sanchuhareesh@gmail.com>
2023-11-21 16:51:34 +0530

    Calculator Mode

---
+++ main.js
@@ -0,0 +1,20 @@
+const displayData = (data)=>{
+    result.value += data
+}
+
+const calculatorClear = ()=>{
+    result.value=""
+}
+
+const calculateResult = ()=>{
+    if(result.value!=""){
+        try{    result.value =  eval(result.value)
+        }catch{
+            result.value = "Error!!!"
+        }
+    }
+}
+
+const removeLst = ()=>{
+    result.value = result.value.slice(0,-1)
+}
